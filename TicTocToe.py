8d7c1a02702ae9c1c3fcf0c64eaf17bfdf745025
A.Görkem ARISOY <79798735+gorkemarisoy@users.noreply.github.com>
2022-09-24 21:39:41 +0200

    Update TicTocToe.py

--- TicTocToe.py
+++ TicTocToe.py
@@ -7,8 +7,7 @@ for r in range(3):
     for c in range(3):
         board[r][c]=counter #to fill playboard from 1to9
         counter += 1
-
-
+list =[5] 
 
 
 
@@ -28,41 +27,72 @@ def display_board(board):
 |       |       |       |
 +-------+-------+-------+
 """)
-    enter_move(board)
+    
 
  
     
 
-def enter_move(board):
+
+def enter_move(board, list):
     print("please make your move 1-9 :")
     me=int(input())
-    for r in range(3):
-        for c in range(3):
-            if board[r][c]== me:
-                board[r][c]="O"
+    if me not in list:
+        for r in range(3):
+            for c in range(3):
+                if board[r][c]== me:
+                    list.append(me)
+                    board[r][c]="O"
+    else:
+        print("try again")
+        return enter_move(board, list)
+    
+    return victory_for(board,"O",list)
+            
+            
+    
                 
+def draw_move(board, list):
     cp = randrange(1,10)
-    for r in range(3):
-        for c in range(3):
-            if board[r][c]==cp:
-                board[r][c]="X"
-    return display_board(board)
+    
+    if cp not in list:
+        for r in range(3):
+            for c in range(3):
+                if board[r][c]==cp :
+                    board[r][c]="X"
+                    list.append(cp)
+    else:
+        return draw_move(board, list)
+    return victory_for(board,"X",list)
 
-board[1][1]="X"            
-display_board(board)        
+    
 
 
             
 
-#def make_list_of_free_fields(board):
-    # The function browses the board and builds a list of all the free squares; 
-    # the list consists of tuples, while each tuple is a pair of row and column numbers.
+def victory_for(board,sign,list):
+    display_board(board)
+    for c in range(3):
+       
+        if (board[c]==[sign,sign,sign] or board[0][c]==[sign,sign,sign] or
+            board[0][0]==sign and board[1][1]==sign and board[2][2]==sign or
+            board[0][2]==sign and board[1][1]==sign and board[2][0]==sign):
+                if sign=="X":
+                    return print("PC won")
+                elif sign=="O":
+                    return print("YOU won")
+        
+    if sign=="X":
+        return enter_move(board, list)
+    else:
+        return draw_move(board, list)
+
+board[1][1]="X"            
+victory_for(board,"X",list)
 
 
-#def victory_for(board, sign):
+
+    
     # The function analyzes the board's status in order to check if 
     # the player using 'O's or 'X's has won the game
 
 
-#def draw_move(board):
-    # The function draws the computer's move and updates the board.
